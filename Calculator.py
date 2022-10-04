1087eeeb3270b2a1e48a367712122c66629f3b62
kunal0411 <114978602+kunal0411@users.noreply.github.com>
2022-10-04 12:05:15 +0530

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,18 @@
+num_1 = int(input('Enter your first number: '))
+num_2 = int(input('Enter your second number: '))
+ 
+# Addition
+print('{} + {} = '.format(num_1, num_2))
+print(num_1 + num_2)
+ 
+# Subtraction
+print('{} - {} = '.format(num_1, num_2))
+print(num_1 - num_2)
+ 
+# Multiplication
+print('{} * {} = '.format(num_1, num_2))
+print(num_1 * num_2)
+ 
+# Division
+print('{} / {} = '.format(num_1, num_2))
+print(num_1 / num_2)
