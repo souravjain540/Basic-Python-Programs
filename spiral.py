60ed480d896105848033b7b6b832d5f45b82a573
Meenakshy Bindu Suresh <meenakshy.b.suresh@gmail.com>
2022-10-05 15:26:36 +0530

    Create spiral.py
    In this code, we created a square spiral pattern using the turtle "Squary." It uses the default colour, which is black. Using the command *squary.color("red")*, you can also alter the colour. You can substitute the desired colour for "red". The turtle forms squares into a spiral by going ahead and rotating left for 91 degrees, then doing this 500 times.

---
+++ spiral.py
@@ -0,0 +1,8 @@
+import turtle
+
+squary = turtle.Turtle()
+squary.speed(10)
+
+for i in range(500): # this "for" loop will repeat these functions 500 times
+    squary.forward(i)
+    squary.left(91)
