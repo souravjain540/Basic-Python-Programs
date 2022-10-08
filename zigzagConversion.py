ced44e6222f36b96e6423b59002887f76507697b
Theedon <faniyantoyin@gmail.com>
2022-10-08 02:13:08 +0100

    add solution to leetcode problem 6 Zigzag Conversion

---
+++ zigzagConversion.py
@@ -0,0 +1,23 @@
+#Leetcode problem 6 Zigzag Conversion
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        L = [''] * numRows
+        index, step = 0, 1
+
+        for x in s:
+            L[index] += x
+            if index == 0:
+                step = 1
+            elif index == numRows -1:
+                step = -1
+            index += step
+
+        return ''.join(L)
