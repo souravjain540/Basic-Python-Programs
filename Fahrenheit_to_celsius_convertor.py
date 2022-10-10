82a347a1f8f3d5e1971aef1996dcfc1125a8a206
Shravanikale <109503478+Shravanikale@users.noreply.github.com>
2022-10-10 22:24:01 +0530

    Create Fahrenheit_to_celsius_convertor.py

---
+++ Fahrenheit_to_celsius_convertor.py
@@ -0,0 +1,16 @@
+Fahrenheit_1 = float( input("Temperature value in degree Fahrenheit: " ))  
+  
+
+celsius_1 = (Fahrenheit_1 - 32)  / 1.8  
+
+
+print ('The %.2f degree Fahrenheit is equal to: %.2f Celsius'  
+      %(Fahrenheit_1, celsius_1))  
+  
+print("----OR----")  
+Fahrenheit_2 = float( input("Temperature value in degree Fahrenheit: " ))  
+celsius_2 = (Fahrenheit_2 - 32) * 5/9  
+    
+
+print ('The %.2f degree Fahrenheit is equal to: %.2f Celsius'  
+      %(Fahrenheit_2, celsius_2))  
