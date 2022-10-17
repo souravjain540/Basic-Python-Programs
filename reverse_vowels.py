73bae942cf908bf8f82fcaf0797f40ecdcfd1826
EliRen7 <84453538+EliRen7@users.noreply.github.com>
2022-10-17 10:25:39 +0900

    Create reverse_vowels.py

---
+++ reverse_vowels.py
@@ -0,0 +1,18 @@
+def reverse_vowels(s):
+    """Reverse vowels in a string.
+    
+    string = list(s)
+    i = 0 
+    j = len(s) - 1
+
+    while i < j:
+        if string[i].lower() not in vowels:
+            i += 1
+        elif string[j].lower() not in vowels:
+            j -= 1
+        else:
+            string[i], string[j] = string[j], string[i]
+            i += 1
+            j -= 1
+
+    return "".join(string)
