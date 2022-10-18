15ecd4f3766eefd5b986391d5f730339a043a66f
himanshu999120 <91904931+himanshu999120@users.noreply.github.com>
2022-10-18 19:34:10 +0530

    Add files via upload

---
+++ pascal triangle.py
@@ -0,0 +1,16 @@
+n = 5
+ 
+for i in range(1, n+1):
+    for j in range(0, n-i+1):
+        print(' ', end='')
+ 
+    # first element is always 1
+    C = 1
+    for j in range(1, i+1):
+ 
+        # first value in a line is always 1
+        print(' ', C, sep='', end='')
+ 
+        # using Binomial Coefficient
+        C = C * (i - j) // j
+    print()
