a3d97ffd17335f3fc85994778e5586af2d9da02a
rohanprichard <rohanprichard@gmail.com>
2022-10-19 18:25:17 +0530

    Kilometers to Miles and vice versa converter
    This is a program to convert kilometers as entered by the user into miles, and another program to do the opposite.

---
+++ miles-to-km-converter.py
@@ -0,0 +1,9 @@
+# Simple program to get input from the user in kilometers and convert it to miles.
+
+
+# User input starts
+def convert():
+    n  = int(input("Enter the distance in Miles: "))
+    print("The distance is", n * 1.60934 ,"Kilometers." )
+
+convert()
