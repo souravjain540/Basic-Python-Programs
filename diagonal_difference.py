2dee7ef744ddd67d6bafad0e749799d09176b88a
DimitriiTrater <dima1603040@gmail.com>
2022-10-21 15:13:46 +0200

    Changes to be committed: 	modified:   diagonal_difference.py

--- diagonal_difference.py
+++ diagonal_difference.py
@@ -1,47 +1,44 @@
 # DimitriiTrater
 
+
 import math
 import os
 import random
 import re
 import sys
 
-# Complete the 'diagonalDifference' function below.
-# The function is expected to return an INTEGER.
-# The function accepts 2D_INTEGER_ARRAY arr as parameter.
-#
 
-def diagonalDifference(arr,n):
+def absolute_difference(matrix,n):
+    """
+    Calculate the absolute difference between the sums of its diagonals.
+
+    :param matrix: square matrix
+    :return: absolute difference between the sums of its diagonals
+    """
     a = []
     b = []
-    #print(arr)
-    k = len(arr[0])-1
+    k = len(matrix[0])-1
     for i in range(n):
-        a.append(arr[i][i])
+        a.append(matrix[i][i])
     for i in range(n):
-        b.append(arr[i][k])
+        b.append(matrix[i][k])
         k -= 1
-    #print(a, b)
     j = sum(a)
-    #print(j)
     m = sum(b)
-    #print(m)
     n = m - j
-    #print(n)
     return abs(n)
      
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     n = int(input().strip())
 
-    arr = []
+    matrix = []
 
     for _ in range(n):
-        arr.append(list(map(int, input().rstrip().split())))
+        matrix.append(list(map(int, input().rstrip().split())))
 
-    result = diagonalDifference(arr, n)
+    result = absolute_difference(matrix, n)
 
     fptr.write(str(result) + '\n')
 
