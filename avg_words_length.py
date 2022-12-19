b7c5b3c0f7b79122ac9d240d23e19fb543324edb
Nathaniel Handan <87228776+Tinny-Robot@users.noreply.github.com>
2022-12-19 15:22:21 +0100

    Create avg_words_length.py

---
+++ avg_words_length.py
@@ -0,0 +1,14 @@
+# For a given sentence, return the average word length. 
+# Note: Remember to remove punctuation first.
+
+sentence1 = "Hi all, my name is Tom...I am originally from Australia."
+sentence2 = "I need to work very hard to learn more about algorithms in Python!"
+
+def solution(sentence):
+    for p in "!?',;.":
+        sentence = sentence.replace(p, '')
+    words = sentence.split()
+    return round(sum(len(word) for word in words)/len(words),2)
+    
+print(solution(sentence1))
+print(solution(sentence2))
