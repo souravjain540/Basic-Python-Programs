6d02c987ae1b577105dd3ac0fcc5e45ca58cefe7
Himanshu Chauhan <codebreaker8055@gmail.com>
2023-01-27 20:13:51 +0530

    Countdown Timer

---
+++ Countdown_Timer.py
@@ -0,0 +1,16 @@
+import time
+
+
+def countdown(time_sec):
+    while time_sec:
+        mins, secs = divmod(time_sec, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
+        print(timeformat, end='\r')
+        time.sleep(1)
+        time_sec -= 1
+
+    print("stop")
+
+
+num = int(input("Set Your timer in Sec : "))
+countdown(num)
