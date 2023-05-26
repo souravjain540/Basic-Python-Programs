94e626dd6466761eec2261adf6a46e951b240966
20016145 <75659608+20016145@users.noreply.github.com>
2023-05-26 10:06:40 +0100

    Update Gobang_Game.py
    add readme_Gobang_game

--- Gobang_Game.py
+++ Gobang_Game.py
@@ -153,3 +153,22 @@ class Gomoku:
 if __name__=="__main__":
     game=Gomoku()
     game.loop()
+    
+    
+    
+  ''' # readme_Gomoku_game
+
+The small game I want to make is called Gomoku. It is a very simple chess game.
+The two sides of the chess are black and white. When one of the chess pieces is connected to 5 pieces. win.
+This game is a two-player battle game. people use the mouse to play chess.
+Human-computer battle is not supported, and they cannot regret chess.
+
+
+First , l start to set up the chessboard. I will explain the size, number and location of the chessboard in the comments.
+
+
+After setting up the chessboard, I will set the color of the chess pieces so that both sides play chess alternately.
+
+
+Then set up the winning and losing mechanism to judge the game.
+When there is a side with 5 players in a line, it is a victory, and the game is over when one side wins.'''
