879b09e416c3a29d69829e0bedd03b628fece823
Jordan <jordanians@yahoo.com>
2023-06-22 15:05:45 -0700

    Took out double turtle and extra line
    I hid the extra turtle that was stationary in the middle of the flag until covered up by the circle that is created later on. Also, the script creates an orange line on the left side of the flag. I lifted the pen during this time to remove that line.

--- Indianflag-Turtle.py
+++ Indianflag-Turtle.py
@@ -1,20 +1,22 @@
 import turtle
 from turtle import*
- 
+
 #screen for output
 screen = turtle.Screen()
- 
+
 # Defining a turtle Instance
+turtle.hideturtle()
 t = turtle.Turtle()
+
 speed(0)
- 
+
 # initially penup()
 t.penup()
 t.goto(-400, 250)
 t.pendown()
- 
+
 # Orange Rectangle
-#white rectangle
+# white rectangle
 t.color("orange")
 t.begin_fill()
 t.forward(800)
@@ -24,8 +26,10 @@ t.right(90)
 t.forward(800)
 t.end_fill()
 t.left(90)
+t.penup()
 t.forward(167)
- 
+t.pendown()
+
 # Green Rectangle
 t.color("green")
 t.begin_fill()
@@ -35,7 +39,7 @@ t.forward(800)
 t.left(90)
 t.forward(167)
 t.end_fill()
- 
+
 # Big Blue Circle
 t.penup()
 t.goto(70, 0)
@@ -44,7 +48,7 @@ t.color("navy")
 t.begin_fill()
 t.circle(70)
 t.end_fill()
- 
+
 # Big White Circle
 t.penup()
 t.goto(60, 0)
@@ -53,7 +57,7 @@ t.color("white")
 t.begin_fill()
 t.circle(60)
 t.end_fill()
- 
+
 # Mini Blue Circles
 t.penup()
 t.goto(-57, -8)
@@ -67,7 +71,7 @@ for i in range(24):
     t.forward(15)
     t.right(15)
     t.pendown()
-     
+
 # Small Blue Circle
 t.penup()
 t.goto(20, 0)
@@ -84,7 +88,7 @@ for i in range(24):
     t.forward(60)
     t.backward(60)
     t.left(15)
-     
+
 #to hold the
 #output window
 turtle.done()
